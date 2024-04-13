af3fa7f470d5cefd9aa62e57216c2ee20c5314c6
dangtrantrung <trung280584@gmail.com>
2024-04-13 10:37:05 +0700

    regex2.findall

--- regex.py
+++ regex.py
@@ -6,3 +6,9 @@ chuoidemo='anh nghia hsgs dep trai dep'
 kq=regex1.search(chuoidemo)
 print(kq.group())
 print(kq.group(1))
+chuoidemo1='ngay dep troi, nang da gap anh nghia hsgs dep trai dep va nang da gap anh trung dep trai'
+regex2=re.compile(r'gap (.*?) dep trai')
+kq1=regex2.search(chuoidemo1)
+print(kq1.group())
+print("ket qua= "+kq1.group(1))
+print(regex2.findall(chuoidemo1))
