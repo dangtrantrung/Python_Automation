beef742d16744a609eccad51dffe7ca18845546d
dangtrantrung <trung280584@gmail.com>
2024-04-18 11:19:03 +0700

    Server is listening

--- Python for Networking/Simple TCP Chat Room/server.py
+++ Python for Networking/Simple TCP Chat Room/server.py
@@ -1,2 +1,45 @@
-import threading
 import socket
+import threading
+
+host='127.0.0.1'
+port=9090
+server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server.bind((host,port))
+server.listen()
+
+clients=[]
+nicknames=[]
+
+def broadcast(message):
+    for client in clients:
+        client.send(message)
+def handle(client):
+    while True:
+        try:
+            message=client.recv(1024)
+            broadcast(message)
+        except:
+            index =clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname=nicknames[index]
+            broadcast(f'{nickname} left the chat room'.encode('ascii'))
+            nicknames.remove(nickname)
+            break
+def receive():
+    while True:
+        client,address=server.accept()
+        print(f'Connectted with {str(address)}')
+        client.send('NICK'.encode('ascii'))
+        nickname=client.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(client)
+        print(f'Nickname of the client is {nickname}!')
+        broadcast(f'{nickname} joined the chat room!'.encode('ascii'))
+        client.send(f'Connected to the server!'.encode('ascii'))
+
+        thread = threading.Thread(target=handle,args=(client,))
+        thread.start()
+
+print('Server is listening...')
+receive()
