6b83def429a5197a37c7246748e05b94bbedc3f4
dangtrantrung <trung280584@gmail.com>
2024-04-21 09:05:35 +0700

    deploy app ngrok

--- tranfser complex python object via socket/client.py
+++ tranfser complex python object via socket/client.py
@@ -1,11 +1,10 @@
 import pickle
-
 import socket
 
 from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
 
 data=load_iris()
 X,y=data.data, data.target
